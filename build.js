de07bf417915eda318c37f9f5ec1db3095e7a82c
trfv <trfv471955@gmail.com>
2021-02-24 23:08:59 +0900

    use watch mode

--- build.js
+++ build.js
@@ -1,9 +1,25 @@
-const { build } = require('esbuild')
-
-build({
-  entryPoints: ['./src/index.tsx'],
-  outfile: './dist/bundle.js',
-  minify: true,
-  bundle: true,
-  define: {'process.env.NODE_ENV': "'production'"}
-}).catch(() => process.exit(1))
+(async () => {
+  const isDev = process.env.NODE_ENV === '"development"';
+  require("fs-extra").copySync("public", "build");
+  require("esbuild")
+    .build({
+      entryPoints: ["src/index.tsx"],
+      outdir: "build",
+      minify: !isDev,
+      watch: isDev,
+      sourcemap: isDev,
+      bundle: true,
+      define: { "process.env.NODE_ENV": process.env.NODE_ENV },
+      // logLevel: "silent",
+    })
+    .catch(() => process.exit(1));
+  if (isDev) {
+    require("live-server").start({
+      root: "build",
+      open: false,
+      host: "localhost",
+      port: 3000,
+      // logLevel: 0,
+    });
+  }
+})();
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App.jsx";
 import "./index.css";
 
-ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
+ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById("root"));
